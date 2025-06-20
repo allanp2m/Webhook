9a649e2915aa4e80a62d5fa41172d43f968eec8c
Allan <106794039+allanp2m@users.noreply.github.com>
2025-06-20 15:44:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log("🚀 Starting server - versão TESTE customizada!");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
